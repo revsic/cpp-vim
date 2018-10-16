06fa298bb639bd95105159530d6ca4191898d206
revsic <revsic99@gmail.com>
2018-10-17 03:15:12 +0900

    Update set.sh - Auto setter

---
+++ set.sh
@@ -0,0 +1,11 @@
+git submodule init
+git submodule update
+
+cp ./.vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir -p ~/.vim/colors/
+mkdir -p ~/.vim/syntax/
+
+cp -r ./jellybeans.vim/colors ~/.vim/colors
+cp -r ./vim-cpp-modern/after/syntax ~/.vim/syntax
+
